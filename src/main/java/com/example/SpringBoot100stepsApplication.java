6290fcab855e28e34606e4de97e215edcb555ae5
chinmayc14 <chinu1400@gmail.com>
2021-08-11 15:21:50 +0530

    first commit

---
+++ src/main/java/com/example/SpringBoot100stepsApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot100stepsApplication {
+
+	// S
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot100stepsApplication.class, args);
+	}
+
+}
